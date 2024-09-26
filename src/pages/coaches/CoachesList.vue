e3538001594d3a78b5f6d134ce287ee2922e7f61
Romnick Sarmiento <135184315+s4rmier@users.noreply.github.com>
2024-09-26 16:12:34 +0800

    -base components -coach store

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,14 +1,55 @@
 <template>
   <section>Filter</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register</router-link>
-    </div>
-    <ul></ul>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found</h3>
+    </base-card>
   </section>
 </template>
 
-<script></script>
+<script>
+import CoachItem from "../../components/coaches/CoachItem.vue";
+export default {
+  components: {
+    CoachItem,
+  },
 
-<style></style>
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters["coaches/coaches"];
+    },
+
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
