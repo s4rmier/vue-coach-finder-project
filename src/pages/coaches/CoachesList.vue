3e4ec6cfe7f50cf02d4d665145a49072670fe816
Romnick Sarmiento <135184315+s4rmier@users.noreply.github.com>
2024-09-26 14:00:05 +0800

    -router -file structure

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>Filter</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register</router-link>
+    </div>
+    <ul></ul>
+  </section>
+</template>
+
+<script></script>
+
+<style></style>
