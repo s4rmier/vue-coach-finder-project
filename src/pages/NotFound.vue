e6f387a93e43ca204dd5c4f5ec50be9e7ef89d28
Romnick Sarmiento <135184315+s4rmier@users.noreply.github.com>
2024-09-27 15:04:42 +0800

    transitions -ok auth -init

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Page not found</h1>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page could not be found</p>
+      <router-link to="/coaches">Coaches</router-link></base-card
+    >
+  </section>
+  >
 </template>
